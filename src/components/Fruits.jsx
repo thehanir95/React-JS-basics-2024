c045aafb96defcf2ed072d1a43c10832c8f05679
Thehani Rathnayaka <thehanicis95@gmail.com>
2024-01-30 15:32:44 +0530

    set multiple data in array

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,10 +1,19 @@
 export default function Fruits() {
-  const fruits = ["apple", "mango", "banana"];
+  const fruits = [
+    { name: "Apple", price: 10, img: "🍎" },
+    { name: "Orange", price: 20, img: "🍊" },
+    { name: "Grapes", price: 10, img: "🍇" },
+    { name: "Mango", price: 10, img: "🥭" },
+    { name: "Banana", price: 10, img: "🍌" },
+  ];
   return (
     <div>
       {fruits.map((fruit) => (
-        <li key={fruit}>{fruit}</li>
+        <li key={fruit.name}>
+            {fruit.name}
+            {fruit.img}
+            </li>
       ))}
     </div>
   );
-} 
+}
